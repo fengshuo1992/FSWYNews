994aa9ded61bae031f7c1f4f368e7c04b9df35d9
fengshuo_1 <963787902@qq.com>
2018-05-07 14:47:14 +0800

    提交123

--- FSWYNews/AppDelegate.m
+++ FSWYNews/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"11222222222");
     // Override point for customization after application launch.
     return YES;
 }
